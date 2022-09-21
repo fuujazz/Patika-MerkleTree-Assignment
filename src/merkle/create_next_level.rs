073cc03ac75c713e8ce4233f329d40de9b53f583
fuujazz <crfuujack@gmail.com>
2022-09-21 15:32:19 +0300

    comments

--- src/merkle/create_next_level.rs
+++ src/merkle/create_next_level.rs
@@ -5,6 +5,7 @@ pub fn create_next_level(current_level: Vec<String>) -> Vec<String> {
     let mut next_level: Vec<String> = Vec::new();
     let mut element = String::new();
 
+    // taking two elements from the current level and hashing them
     for (index, item) in current_level.iter().enumerate() {
         if index % 2 == 0 {
             element = item.clone();
--- src/merkle/merkle_root.rs
+++ src/merkle/merkle_root.rs
@@ -3,18 +3,22 @@ use super::hash_input::hash_input;
 use std::fs;
 
 pub fn merkle_root(filename: String) -> String {
+    // reading data and creating a result vector that contains the informations as String
     let result_vector = fs::read_to_string(filename)
         .expect("Unable to read file")
         .lines()
         .map(|line| line.to_string())
         .collect::<Vec<String>>();
 
+    // lenght of the leaves that is read from the file
     let lenght_of_leaves: u32 = result_vector[0].parse().unwrap();
 
+    // data that is read from the file
     let mut data = result_vector[1..].to_vec();
 
     let mut hex_vector: Vec<String> = Vec::new();
 
+    // hashing the data and creating next level
     for _ in 0..lenght_of_leaves {
         hex_vector = data.iter().map(|element| hash_input(element)).collect();
         data = create_next_level(hex_vector);
